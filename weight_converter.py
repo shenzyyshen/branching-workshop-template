107ca429da914815d4cf692874558d99066f8f80
Giouxel Kasaika <uxell@b9450176-2444-430f-a819-2b6a12110b76.fritz.box>
2025-05-28 13:11:30 +0200

    fixed error from comment

--- weight_converter.py
+++ weight_converter.py
@@ -1,4 +1,4 @@
 def grams_to_kilograms(grams):
-   """This function returns Kilogram"""
-    kilograms = grams / 1000
+    """This function returns Kilogram"""
+    kilograms = grams // 1000
     return kilograms
